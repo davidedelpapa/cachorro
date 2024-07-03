af4e147fb2f75b4b3cf3b7f892d3648e1edbf2c3
Davide Del Papa <davidedelpapa@yahoo.com>
2024-07-03 16:40:14 +0200

    Added load_cache

--- cachorro/__init__.py
+++ cachorro/__init__.py
@@ -1,7 +1,10 @@
 """Provides the default imports for the cachorro library."""
 from .decorators import cacheme
-from .utils import get_cache_filepath, clear_cache
+from .utils import get_cache_filepath, clear_cache, load_cache
 
 
 VERSION = "0.0.1"
-__all__ = ['cacheme', 'get_cache_filepath', 'clear_cache', 'VERSION']
+__all__ = [
+    'cacheme', 'get_cache_filepath', 'clear_cache',
+    'load_cache', 'VERSION'
+]
--- cachorro/utils.py
+++ cachorro/utils.py
@@ -1,6 +1,7 @@
 """Utilities for the library."""
 import __main__
 import os
+import pickle
 import logging
 
 
@@ -60,6 +61,49 @@ def clear_cache(func_name, program_name=None):
     filepath = get_cache_filepath(func_name, program_name)
     if os.path.exists(filepath):
         os.remove(filepath)
-        log.info(f"Cache cleared for {__main__.__file__}: {func_name}")
+        log.info(f"Cache cleared for {program_name}: {func_name}")
     else:
-        log.info(f"No cache found for {__main__.__file__}: {func_name}")
+        log.info(f"No cache found for {program_name}: {func_name}")
+
+
+def load_cache(func_name, program_name=None):
+    """Load a specific funcion's cached result.
+
+    This function loads a cached result from a file.
+    It first checks if the `program_name` is provided, and if not,
+    it infers it from the main file name. It then loads the cached result.
+    If the loading fails, removes the cache file, and returns None
+
+    Args:
+        func_name (str): The name of the function to load the cache for.
+        program_name (str, optional): The name of the program.
+            If not provided, it will be inferred from the main file name.
+
+    Returns:
+        The cached result if found and successfully loaded,
+            or None if the cache file does not exist or fails to load.
+
+    Raises:
+        None
+
+    Example:
+        >>> load_cache('my_function', program_name='my_program')
+        Loading saved state for my_program: my_function
+        <cached_result>
+    """
+    if program_name is None:
+        program_name = os.path.splitext(os.path.basename(__main__.__file__))[0]
+    filepath = get_cache_filepath(func_name, program_name)
+    if os.path.exists(filepath):
+        try:
+            with open(filepath, 'rb') as file:
+                complete_name = f"{program_name}: {func_name}"
+                log.info(f"Loading saved state for {complete_name}")
+                return pickle.load(file)
+        except Exception as e:
+            log.critical(f"Failed to load saved state for {func_name}: {e}")
+            os.remove(filepath)  # remove faulty pickle file
+            return None
+    else:
+        log.info(f"No cache found for {func_name}")
+        return None
